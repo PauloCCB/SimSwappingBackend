a098ef31c9369160bacd7270142665858849e004
PauloCCB <paulo.napasa3@gmail.com>
2025-07-31 22:01:49 -0500

    First

--- src/main/java/com/simswapping/dao/SimSwappingDao.java
+++ src/main/java/com/simswapping/dao/SimSwappingDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface SimSwappingDao {
 
-    Integer registerAccount(BodyAccount bodyAccount);
+    Integer registerAccount(BodyAccount bodyAccount); 
 
     List<Usuario> login(BodyLogin bodyLogin) throws Exception;
 
